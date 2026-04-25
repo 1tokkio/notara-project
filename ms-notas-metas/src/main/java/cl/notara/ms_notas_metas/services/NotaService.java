e32a45d46abb2da93d0603d1ef4201b45d4ed818
EliecerSH <elie.salgado@duocuc.cl>
2026-04-25 01:42:10 -0400

    integracion de clase DTO

--- ms-notas-metas/src/main/java/cl/notara/ms_notas_metas/services/NotaService.java
+++ ms-notas-metas/src/main/java/cl/notara/ms_notas_metas/services/NotaService.java
@@ -1,5 +1,7 @@
 package cl.notara.ms_notas_metas.services;
 
+import cl.notara.ms_notas_metas.client.UsuarioClient;
+import cl.notara.ms_notas_metas.dto.UsuarioDTO;
 import cl.notara.ms_notas_metas.exceptions.ResourceNotFoundException;
 import cl.notara.ms_notas_metas.models.Nota;
 import cl.notara.ms_notas_metas.repositories.NotaRepository;
@@ -11,9 +13,11 @@ import java.util.List;
 public class NotaService {
 
     private final NotaRepository notaRepository;
+    private final UsuarioClient usuarioCliente;
 
-    public NotaService(NotaRepository notaRepository) {
+    public NotaService(NotaRepository notaRepository, UsuarioClient usuarioCliente) {
         this.notaRepository = notaRepository;
+        this.usuarioCliente = usuarioCliente;
     }
 
     public List<Nota> listar() {
@@ -21,6 +25,18 @@ public class NotaService {
     }
 
     public Nota guardar(Nota nota) {
+        try {
+            UsuarioDTO user = usuarioCliente.getUsuario(nota.getIdUsuario());
+
+            if (user == null) {
+                throw new ResourceNotFoundException("El usuario con ID " + nota.getIdUsuario() + " no fue encontrado en el sistema externo.");
+            }
+
+            System.out.println("Creando nota para el usuario: " + user.getNombre());
+
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("Error al validar usuario: El ID " + nota.getIdUsuario() + " no existe o el servicio no está disponible.");
+        }
         return notaRepository.save(nota);
     }
 
